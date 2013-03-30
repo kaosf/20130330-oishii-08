93e3103aa9fb81263ace69e3a2243e486e9fece3
ka <ka.kaosf@gmail.com>
2013-03-30 15:09:52 +0900

    add the process to get Button

--- src/com/example/helloworld/MainActivity.java
+++ src/com/example/helloworld/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.helloworld;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button1);
     }
 
 
